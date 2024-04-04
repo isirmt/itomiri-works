7f44192b2745d38aecc50a7a18c1e6af71eedb7e
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-04-05 01:17:11 +0900

    update: woeic stg downloading

--- src/pages/woeic_stg/index.jsx
+++ src/pages/woeic_stg/index.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-page-custom-font */
-import * as React from 'react'
-import UnderDevelopment from '@/components/UnderDevelopment'
-import SEO from '@/components/SEO'
-import styles from '@/styles/pages/woeic_stg.module.scss'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBackward } from "@fortawesome/free-solid-svg-icons"
+import * as React from "react";
+import UnderDevelopment from "@/components/UnderDevelopment";
+import SEO from "@/components/SEO";
+import styles from "@/styles/pages/woeic_stg.module.scss";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBackward } from "@fortawesome/free-solid-svg-icons";
 
 export default class WoeicMain extends React.Component {
   constructor() {
@@ -27,11 +27,13 @@ export default class WoeicMain extends React.Component {
   }
 
   render() {
-
     return (
       <>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <SEO
           pageTitle="WOEIC STG"
@@ -43,15 +45,17 @@ export default class WoeicMain extends React.Component {
             <div>
               <Link href="../" className={styles.back_link}>
                 <span className={styles.back_button}>
-                  <FontAwesomeIcon icon={faBackward}
-                    className={styles.back_icon} />
+                  <FontAwesomeIcon
+                    icon={faBackward}
+                    className={styles.back_icon}
+                  />
                 </span>
               </Link>
               <div className={styles.header_icon}>
                 <Image
                   src="/woeic_stg/woeic_icon.png"
                   fill
-                  alt='woeic_icon'
+                  alt="woeic_icon"
                   className={styles.header_icon_img}
                 />
               </div>
@@ -64,47 +68,90 @@ export default class WoeicMain extends React.Component {
           <div className={styles.top_banner_caption_1}>Learning</div>
           <div className={styles.top_banner_caption_2}>TOEIC&reg; Words</div>
           <div className={styles.top_banner_caption_3}>with Shooting Game</div>
-          <div className={styles.top_banner_icon}><Image
-            src="/woeic_stg/woeic_icon.png"
-            fill
-            alt='woeic_icon'
-            className={styles.top_banner_icon_img} /></div>
+          <div className={styles.top_banner_icon}>
+            <Image
+              src="/woeic_stg/woeic_icon.png"
+              fill
+              alt="woeic_icon"
+              className={styles.top_banner_icon_img}
+            />
+          </div>
         </div>
         <div className={styles.blank} />
         <div className={styles.whatis_banner}>
           <div className={styles.whatis_title}>What is WOEIC STG?</div>
-          <div className={styles.whatis_caption}>Learning while <span>playing</span></div>
-          <div className={styles.whatis_caption}>Judging words in <span>shooting games</span></div>
-          <div className={styles.whatis_caption}>Check the <span>wrong word</span> at the end of the game</div>
+          <div className={styles.whatis_caption}>
+            Learning while <span>playing</span>
+          </div>
+          <div className={styles.whatis_caption}>
+            Judging words in <span>shooting games</span>
+          </div>
+          <div className={styles.whatis_caption}>
+            Check the <span>wrong word</span> at the end of the game
+          </div>
         </div>
         <div className={styles.download_banner}>
-          <p className={styles.title}>Play on PC</p>
-          <p className={styles.caption}>You may need <Link target="_blank" rel="noopener noreferrer" href="https://www.java.com/download/"><span>Java</span></Link> runtime to play.</p>
-          <Link href="" style={{ textDecoration: "none" }} className={styles.download_link}>
-            <div className={styles.download_button}>
-              Download Now
-            </div>
+          <Link
+            href="#downloads"
+            style={{ textDecoration: "none" }}
+            className={styles.download_link}
+          >
+            <div className={styles.download_button}>Download Now</div>
           </Link>
-          <p className={styles.title}>Downloads</p>
+          <p className={styles.title} id="downloads">
+            Downloads
+          </p>
           <ul className={styles.vlist}>
-            <li>Version 1.0
+            <li>
+              Version 1.0
               <ul>
-                <Link href="/woeic_stg/v1.0/WOEIC_STG-Windows-v1.0.zip" download="WOEIC_STG-Windows-v1.0.zip"><li>Windows</li></Link>
+                <Link
+                  href="https://drive.google.com/file/d/1NGC8WBbxbwwKv-uNrzH-fyZHx5ojJQkS/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <li>Windows</li>
+                </Link>
                 {/* <Link href=""><li>Mac OS</li></Link> */}
-                <Link href="/woeic_stg/v1.0/WOEIC_STG-Linux-v1.0.zip"  download="WOEIC_STG-Linux-v1.0.zip"><li>Linux</li></Link>
+                <Link
+                  href="https://drive.google.com/file/d/1MFS7JehnIw8tqgUvrjF-Q5Le5_cPWmZM/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <li>Linux</li>
+                </Link>
               </ul>
             </li>
           </ul>
           <p className={styles.title}>Thanks</p>
-          <p className={styles.scaption}>The Words in this game are taken from&nbsp;
+          <p className={styles.scaption}>
+            The Words in this game are taken from&nbsp;
             <Link
-              target="_blank" rel="noopener noreferrer"
-              href="https://quizlet.com/ja/content/toeic?utm_source=student-product-feature&utm_medium=email&utm_campaign=toeic-test">
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://quizlet.com/ja/content/toeic?utm_source=student-product-feature&utm_medium=email&utm_campaign=toeic-test"
+            >
               <span>Quizlet</span>.
-            </Link></p>
+            </Link>
+          </p>
           <p className={styles.scaption}>Thank you so much.</p>
+          <p className={styles.title}>Play on PC</p>
+          <p className={styles.caption}>
+            You may need{" "}
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.oracle.com/java/technologies/downloads/"
+            >
+              <span>JDK</span>
+            </Link>{" "}
+            to play.
+            <br />
+            <br />
+            (Windows version is bundled with JDK17 by default for running)
+          </p>
         </div>
       </>
-    )
+    );
   }
 }
